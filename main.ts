cfd8dffceda8e782ff2bfbc0eb08fcd7cca42ab0
richardklapwijk <110915049+richardklapwijk@users.noreply.github.com>
2022-08-26 21:33:01 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(200)
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    basic.pause(200)
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(200)
+    for (let index = 0; index < 20; index++) {
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        basic.pause(200)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 20; index++) {
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        basic.pause(100)
+        music.playMelody("- B A A G - E E ", 120)
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        basic.pause(50)
+    }
 })
