76db521c167d0fa53ae3f9290b4a755c11b1321c
richardklapwijk <110915049+richardklapwijk@users.noreply.github.com>
2022-08-29 21:18:58 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 20; index++) {
+        media.sendCode(media.keys(media._MediaKey.playPause))
         pins.digitalWritePin(DigitalPin.P13, 1)
         basic.pause(500)
         pins.digitalWritePin(DigitalPin.P13, 0)
         basic.pause(200)
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    media.sendCode(media.keys(media._MediaKey.previous))
+})
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 20; index++) {
+        media.sendCode(media.keys(media._MediaKey.next))
         pins.digitalWritePin(DigitalPin.P13, 1)
         basic.pause(1000)
         pins.digitalWritePin(DigitalPin.P13, 0)
@@ -20,3 +25,4 @@ input.onButtonPressed(Button.B, function () {
         basic.pause(50)
     }
 })
+media.startMediaService()
